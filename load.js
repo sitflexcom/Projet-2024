c6ab3f2ff9942be64ff9a3daca863d1fc57c918d
sitflexcom <johnfredstanley2@gmail.com>
2024-09-14 21:21:51 -0400

    Add files via upload
    Signed-off-by: sitflexcom <johnfredstanley2@gmail.com>

---
+++ load.js
@@ -0,0 +1,14 @@
+// Created by Only Alya 🍁🇺🇸
+
+
+
+window.addEventListener('load', () => {
+
+    setTimeout(() => {
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('slpp').style.display = 'block';
+    }, 3500);
+});
+
+alert(" Wait 3-4 seconds 😉👍🏻 "
+);
